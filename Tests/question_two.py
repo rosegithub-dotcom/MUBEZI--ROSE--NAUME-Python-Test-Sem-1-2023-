82d199b1dff2b1c0a3df878becfa664da786ca07
rosegithub-dotcom <143061942+rosegithub-dotcom@users.noreply.github.com>
2023-10-19 14:32:52 +0300

    Update question_two.py

--- Tests/question_two.py
+++ Tests/question_two.py
@@ -5,3 +5,17 @@ def average_of_two_numbers(x,y):
 
 first_number=6
 second_number=4
+
+number1 = int(input("Enter first_number:"))
+number2 = int(input("enter second_ number:"))
+number3 = int(input("enter third_number:"))
+if number1<number2 and number1<number3:
+    print(f"{number1} is less than {number2} and {number3}")
+elif number2<number1 and number2<number3:
+        print(f"{number2} is less than {number1} and {number3}")
+else:
+    print(f"{number3} is less than {number1} and {number2}")
+
+
+
+
